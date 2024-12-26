95e105ddef5c37ca994f29c74c50a937036dc84a
Atif Khan <atif.shivamarts@gmail.com>
2024-12-27 01:48:59 +0530

    form update

--- forms/appointment.php
+++ forms/appointment.php
@@ -8,38 +8,21 @@
 
   // Replace contact@example.com with your real receiving email address
   $receiving_email_address = 'vivekchauhan2734@gmail.com';
-
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
-
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
+  $to = $receiving_email_address;
+  $name = $_POST['name'];
+  $from = $_POST['email'];
+  $subject = 'Online Appointment Form';
+  $message = 'Deptt' . $_POST['department'] . 'doctor' .  $_POST['doctor'] .  $_POST['date'] .'Appointment Date' ;
   
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = 'Online Appointment Form';
-
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
-  $contact->smtp = array(
-    'host' => 'example.com',
-    'username' => 'example',
-    'password' => 'pass',
-    'port' => '587'
-  );
-  */
-
-  $contact->add_message( $_POST['name'], 'Name');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['phone'], 'Phone');
-  $contact->add_message( $_POST['date'], 'Appointment Date');
-  $contact->add_message( $_POST['department'], 'Department');
-  $contact->add_message( $_POST['doctor'], 'Doctor');
-  $contact->add_message( $_POST['message'], 'Message');
-
-  echo $contact->send();
+  if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
+      $headers = ['From' => ($name?"<$name> ":'').$from,
+              'X-Mailer' => 'PHP/' . phpversion()
+             ];
+  
+      mail($to, $subject, $message."\r\n\r\nfrom: ".$_SERVER['REMOTE_ADDR'], $headers);
+      die('OK');
+      
+  } else {
+      die('Invalid address');
+  }
 ?>
